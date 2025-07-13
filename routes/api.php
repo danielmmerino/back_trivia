e01cd2bbca659ddacc5d8f85f389104b016cdd57
danielmmerino <danielm_merino@hotmail.com>
2025-07-12 23:24:54 -0500

    Add niveles endpoint

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Middleware\TokenAuth;
 use App\Http\Middleware\CheckApiKey;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LevelController;
 
 Route::post('/login', [AuthController::class, 'login'])->middleware(CheckApiKey::class);
 Route::post('/login-usuarios', [AuthController::class, 'loginUsuarios'])->middleware(CheckApiKey::class);
 Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
 
 Route::get('/categories', [CategoryController::class, 'index'])
+    ->middleware(TokenAuth::class);
+Route::get('/niveles', [LevelController::class, 'index'])
     ->middleware(TokenAuth::class);
 
 Route::middleware(TokenAuth::class)->group(function () {
