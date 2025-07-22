6d3d9089f518aadb25517cb878e0f259a5fe070f
danielmmerino <danielm_merino@hotmail.com>
2025-07-21 20:38:29 -0500

    Return JSON errors for API routes

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $e)
+    {
+        if ($request->expectsJson() || $request->is('api/*')) {
+            $status = $this->isHttpException($e) ? $e->getStatusCode() : 500;
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], $status);
+        }
+
+        return parent::render($request, $e);
+    }
+}
